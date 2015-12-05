ebb3612d66b97fa61dca9c82e07fb3cdda70d400
franciscofranco <franciscofranco.1990@gmail.com>
2015-12-05 04:01:17 +0000

    arm64: use the new *_relaxed macros for lower power usage
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Conflicts:
    	arch/arm64/kernel/ptrace.c
    	kernel/irq_work.c
    	kernel/mutex.c
    	kernel/sched/core.c
    	kernel/smp.c
    	mm/memcontrol.c

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4634,7 +4634,7 @@ unsigned long wait_task_inactive(struct task_struct *p, long match_state)
 		 */
 		while (task_running(rq, p)) {
 			if (match_state && unlikely(cpu_relaxed_read_long
- 				(&(p->state)) != match_state))
+				(&(p->state)) != match_state))
 				return 0;
 			cpu_read_relax();
 		}
@@ -4976,7 +4976,7 @@ void scheduler_ipi(void)
 	 */
 	preempt_fold_need_resched();
 
-	if (llist_empty(&this_rq()->wake_list) && !got_nohz_idle_kick() &&
+	if (llist_empty_relaxed(&this_rq()->wake_list) && !got_nohz_idle_kick() &&
 							!got_boost_kick())
 		return;
 
@@ -6936,7 +6936,7 @@ int idle_cpu(int cpu)
 		return 0;
 
 #ifdef CONFIG_SMP
-	if (!llist_empty(&rq->wake_list))
+	if (!llist_empty_relaxed(&rq->wake_list))
 		return 0;
 #endif
 
