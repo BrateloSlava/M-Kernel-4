7d3dc080056eadcca994781f29310ace0d3d8960
Slawek <yarpiin@gmail.com>
2016-12-09 23:59:04 +0000

    Add latest impulse governor
    Change-Id: I55c8d7bc1ddd8265685de9fd591c264e296fdc6e

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -521,6 +521,9 @@ extern struct cpufreq_governor cpufreq_gov_blu_active;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NEBULA)
 extern struct cpufreq_governor cpufreq_gov_nebula;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nebula)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
 #endif
 
 /*********************************************************************
