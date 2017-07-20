828ffc2152706199b4e936ed7f8c6268913b44fe
BrateloSlava <d_slava@list.ru>
2017-07-20 22:56:16 +0300

    ASMP: increase startup delay

--- arch/arm64/hotplug/autosmp.c
+++ arch/arm64/hotplug/autosmp.c
@@ -42,7 +42,7 @@
 #define HOTPLUG_ENABLED 0
 #define DEBUG 0
 #define ASMP_TAG "AutoSMP: "
-#define ASMP_STARTDELAY 20000
+#define ASMP_STARTDELAY 40000
 
 struct asmp_cpudata_t {
 	long long unsigned int times_hotplugged;
